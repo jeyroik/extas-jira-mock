2a1982376a9e558fd575581c2e4ecdbdc76ee20a
jeyroik <jeyroik@gmail.com>
2020-05-26 16:56:57 +0300

    added dotenv

--- src/web/index.php
+++ src/web/index.php
@@ -1,7 +1,13 @@
 <?php
 require(__DIR__ . '/../../vendor/autoload.php');
+define('APP__ROOT', getenv('EXTAS__BASE_PATH') ?: __DIR__ . '/../..');
 
 use extas\components\jira\MockServer;
+
+if (is_file(APP__ROOT . '/.env')) {
+    $dotenv = \Dotenv\Dotenv::create(APP__ROOT . '/');
+    $dotenv->load();
+}
 
 $server = new MockServer( [
     MockServer::FIELD__HOST => getenv('EXTAS__JIRA_MOCK__HOST') ?: 'http://localhost',
