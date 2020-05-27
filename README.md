ff9b997fae0225c49c65ad29adebfb5ac4abbbbb
jeyroik <jeyroik@gmail.com>
2020-05-27 14:00:47 +0300

    added badges

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# extas-jira-mock
-Mock Jira application by Extas
+![tests](https://github.com/jeyroik/extas-jira-mock/workflows/PHP%20Composer/badge.svg?branch=master&event=push)
+![codecov.io](https://codecov.io/gh/jeyroik/extas-jira-mock/coverage.svg?branch=master)
+<a href="https://github.com/phpstan/phpstan"><img src="https://img.shields.io/badge/PHPStan-enabled-brightgreen.svg?style=flat" alt="PHPStan Enabled"></a> 
+<a href="https://codeclimate.com/github/jeyroik/extas-jira-mock/maintainability"><img src="https://api.codeclimate.com/v1/badges/984793c4ec4201f5824e/maintainability" /></a>
+
+# Описание
+
+Мок-сервер Jira. Эмулирует работу Jira.
+
+# Использование
+
+## Запускаем сервер
+
+`extas-jira-mock# php -S 0.0.0.0:8080 -t src/web`
+
+## Шлём запросы
+
+`# curl localhost:8080/rest/api/2/issue`
--- tests/PluginJiraMockRoutesTest.php
+++ tests/PluginJiraMockRoutesTest.php
@@ -2,8 +2,15 @@
 namespace tests;
 
 use Dotenv\Dotenv;
+use extas\components\extensions\TSnuffExtensions;
+use extas\components\http\TSnuffHttp;
+use extas\components\jira\JiraRoute;
+use extas\components\jira\JiraRouteRepository;
+use extas\components\jira\routes\RoutePrepared;
 use extas\components\jsonrpc\App;
 use extas\components\plugins\jsonrpc\PluginJiraMockRoutes;
+use extas\interfaces\repositories\IRepository;
+use extas\interfaces\samples\parameters\ISampleParameter;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -14,11 +21,26 @@ use PHPUnit\Framework\TestCase;
  */
 class PluginJiraMockRoutesTest extends TestCase
 {
+    use TSnuffHttp;
+    use TSnuffExtensions;
+
+    protected IRepository $routeRepo;
+
     protected function setUp(): void
     {
         parent::setUp();
         $env = Dotenv::create(getcwd() . '/tests/');
         $env->load();
+        $this->routeRepo = new JiraRouteRepository();
+        $this->addReposForExt([
+            'jiraRouteRepository' => JiraRouteRepository::class
+        ]);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->routeRepo->delete([JiraRoute::FIELD__NAME => '/test']);
+        $this->deleteSnuffExtensions();
     }
 
     public function testPlugin()
@@ -33,5 +55,49 @@ class PluginJiraMockRoutesTest extends TestCase
          * - /
          */
         $this->assertCount(3, $routes);
+    }
+
+    public function testPluginRun()
+    {
+        $app = App::create();
+        $plugin = new PluginJiraMockRoutes();
+        $plugin($app);
+        $routes = $app->getRouteCollector()->getRoutes();
+        $this->createRoute('/prepared.json', RoutePrepared::class);
+
+        foreach ($routes as $route) {
+            if ($route->getPattern() == '/') {
+                $dispatcher = $route->getCallable();
+                $response = $dispatcher(
+                    $this->getPsrRequest('', [], '', 'GET', '/test'),
+                    $this->getPsrResponse(),
+                    []
+                );
+
+                $this->assertEquals(
+                    '{"test": "is ok"}',
+                    (string) $response->getBody(),
+                    'Response mismatched: ' . $response->getBody()
+                );
+            }
+        }
+    }
+
+    /**
+     * @param string $path
+     * @param string $class
+     */
+    protected function createRoute(string $path, string $class): void
+    {
+        $this->routeRepo->create(new JiraRoute([
+            JiraRoute::FIELD__NAME => '/test',
+            JiraRoute::FIELD__CLASS => $class,
+            JiraRoute::FIELD__PARAMETERS => [
+                'path' => [
+                    ISampleParameter::FIELD__NAME => 'path',
+                    ISampleParameter::FIELD__VALUE => $path
+                ]
+            ]
+        ]));
     }
 }
